8a0ae82fa714b43701e835cb7a98c3c82ad7d7c4
Wellington Guimaraes <112776793+WellyngthonG@users.noreply.github.com>
2022-09-15 11:51:40 -0300

    Add files via upload

---
+++ excel_com_pandas.py
@@ -0,0 +1,12 @@
+from ast import main
+from telnetlib import STATUS
+import pandas as pd
+
+carros = {
+    'marca':['Fiat', 'Chevrolet', 'Ford'],
+    'modelo':['Marea', 'Chevette', 'Escort'],
+    'ano':['1999', '1978', '1995'],
+}
+
+dataframe = pd.DataFrame(carros)
+dataframe.to_excel('./carros.xlsx')
